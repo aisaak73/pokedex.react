ca06d2e0f3986d96e8c0bd9d4c54b0db3ebe9f64
Isaak Antunez <isaakantunez73@gmail.com>
2021-06-11 20:45:57 -0600

    init

---
+++ src/components/screens/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+const Home = () =>{
+    return (
+        <View>
+            <Text> Pokedex con React Native y PokeAPi</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({});
+
+export default Home;
